82cb4844f5e6233de554c093f1d5be41f4e6aa8d
Thackerie <thackerie164@gmail.com>
2023-01-26 10:08:44 -0500

    page frmework finished and initial models written

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,32 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    CATEGORIES = [
+        ("1","Category 1"),
+        ("2", "Category 2"),
+        ("3", "Category 3")
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Listings")
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=200)
+    creation_date = models.DateTimeField()
+    #image = models.ImageField()
+
+    #multiple categories somehow
+    category = models.CharField(max_length=64, choices=CATEGORIES)
+
+    starting_price = models.FloatField()
+    #use Listing.Comments.all() to get all related comments
+    watchlisted = models.ManyToManyField(User, related_name="Watchlist", null=True)
+    
+class Bid(models.Model):
+    amount= models.FloatField()
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="Bids")
+    
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Comments")
+    content = models.CharField(max_length=200)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="Comments")
